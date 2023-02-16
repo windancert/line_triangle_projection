6482f5f4b64ca60f0bd477109f7050f9997c539a
rolandblok <rolandblok@gmail.com>
2023-02-16 08:55:44 +0100

    donno

--- line_2d3d/cpp/line_2d3d/line_2d3d/MyPaths.cpp
+++ line_2d3d/cpp/line_2d3d/line_2d3d/MyPaths.cpp
@@ -53,56 +53,69 @@ MyPaths::MyPaths(vector<MyLine> &visible_lines)
     cout << "Culling double lines to " << visible_lines.size() << "\n";
 
     
+    bool no_more_connections_found = true;
     while (visible_lines.size() > 0) {
-        paths_list.push_back(list<Vector3d>());        // create a line list and put it to the path list
-        list<Vector3d>& point_path = paths_list.back(); // get a reference to add lines to. (avoid copying whole lists).
 
+        if (no_more_connections_found) {
+            paths_list.push_back(list<Vector3d>());        // create a line list and put it to the path list
+            list<Vector3d>& point_path = paths_list.back(); // get a reference to add lines to. (avoid copying whole lists).
+
+            auto first_line_it = visible_lines.begin();
+            c = first_line_it->c;
+            thickness = first_line_it->thickness;
+            point_path.push_back(first_line_it->ps[0]);  // copy the points of the first line
+            point_path.push_back(first_line_it->ps[1]);
+            visible_lines.erase(first_line_it);            // remove it from the list, and go for the next one
+        }
+        else {
+            no_more_connections_found = true;
+        }
+
+
+        list<Vector3d>& point_path = paths_list.back();
         auto check_line_it = visible_lines.begin(); // make an iterator over the visible lines
-        c = check_line_it->c;
-        thickness = check_line_it->thickness;
-        point_path.push_back(check_line_it->ps[0]);  // copy the points of the first line
-        point_path.push_back(check_line_it->ps[1]);
-        check_line_it = visible_lines.erase(check_line_it);         // remove it from the list, and go for the next one
-
-        if (visible_lines.size() > 0) {            // if there are more lines, let's check'm
-            while (check_line_it != visible_lines.end()) {
-                // see if the next line connects to the start or end.
-                Vector3d& start = point_path.front();       // get a reference to the front (for readability)
-                Vector3d& end = point_path.back();         // get a reference to the back (for readability)
+        while (check_line_it != visible_lines.end()) {
+            // see if the next line connects to the start or end.
+            Vector3d& start = point_path.front();       // get a reference to the front (for readability)
+            Vector3d& end = point_path.back();         // get a reference to the back (for readability)
 
 
-                if (floatEqualsRelative(start.x(), check_line_it->ps[0].x(), FLOATING_POINT_ACCURACY) &&
-                    floatEqualsRelative(start.y(), check_line_it->ps[0].y(), FLOATING_POINT_ACCURACY)) {
-                    // start connects to left (0) of iterated line
-                    point_path.push_front(check_line_it->ps[1]);
-                    check_line_it = visible_lines.erase(check_line_it);
-
-                }
-                else if (floatEqualsRelative(start.x(), check_line_it->ps[1].x(), FLOATING_POINT_ACCURACY) &&
-                    floatEqualsRelative(start.y(), check_line_it->ps[1].y(), FLOATING_POINT_ACCURACY)) {
-                    // start connects to right of iterated line
-                    point_path.push_front(check_line_it->ps[0]);
-                    check_line_it = visible_lines.erase(check_line_it);
-
-                }
-                else if (floatEqualsRelative(end.x(), check_line_it->ps[0].x(), FLOATING_POINT_ACCURACY) &&
-                    floatEqualsRelative(end.y(), check_line_it->ps[0].y(), FLOATING_POINT_ACCURACY)) {
-                    // start connects to left of iterated line
-                    point_path.push_back(check_line_it->ps[1]);
-                    check_line_it = visible_lines.erase(check_line_it);
-
-                }
-                else if (floatEqualsRelative(end.x(), check_line_it->ps[1].x(), FLOATING_POINT_ACCURACY) &&
-                    floatEqualsRelative(end.y(), check_line_it->ps[1].y(), FLOATING_POINT_ACCURACY)) {
-                    // start connects to right of iterated line
-                    point_path.push_back(check_line_it->ps[0]);
-                    check_line_it = visible_lines.erase(check_line_it);
-
-                }
-                else {
-                    check_line_it++;
-                }
+            if (floatEqualsRelative(start.x(), check_line_it->ps[0].x(), FLOATING_POINT_ACCURACY) &&
+                floatEqualsRelative(start.y(), check_line_it->ps[0].y(), FLOATING_POINT_ACCURACY)) {
+                // start connects to left (0) of iterated line
+                point_path.push_front(check_line_it->ps[1]);
+                check_line_it = visible_lines.erase(check_line_it);
+                no_more_connections_found = false;
+                break;
             }
+            else if (floatEqualsRelative(start.x(), check_line_it->ps[1].x(), FLOATING_POINT_ACCURACY) &&
+                floatEqualsRelative(start.y(), check_line_it->ps[1].y(), FLOATING_POINT_ACCURACY)) {
+                // start connects to right of iterated line
+                point_path.push_front(check_line_it->ps[0]);
+                check_line_it = visible_lines.erase(check_line_it);
+                no_more_connections_found = false;
+                break;
+            }
+            else if (floatEqualsRelative(end.x(), check_line_it->ps[0].x(), FLOATING_POINT_ACCURACY) &&
+                floatEqualsRelative(end.y(), check_line_it->ps[0].y(), FLOATING_POINT_ACCURACY)) {
+                // start connects to left of iterated line
+                point_path.push_back(check_line_it->ps[1]);
+                check_line_it = visible_lines.erase(check_line_it);
+                no_more_connections_found = false;
+                break;
+            }
+            else if (floatEqualsRelative(end.x(), check_line_it->ps[1].x(), FLOATING_POINT_ACCURACY) &&
+                floatEqualsRelative(end.y(), check_line_it->ps[1].y(), FLOATING_POINT_ACCURACY)) {
+                // start connects to right of iterated line
+                point_path.push_back(check_line_it->ps[0]);
+                check_line_it = visible_lines.erase(check_line_it);
+                no_more_connections_found = false;
+                break;
+            }
+            else {
+                check_line_it++;
+            }
+
         }
     }
     
--- processing_read_svg/processing_read_svg.pde
+++ processing_read_svg/processing_read_svg.pde
@@ -4,15 +4,16 @@ XML xml;
 
 Vector<Vector<PVector>> paths;
 
+
 void setup() {
   size(1400,1400);
   xml = loadXML("../line_2d3d/cpp/line_2d3d/line_2d3d/svg_pathed.svg");
+  //xml = loadXML("roland.svg");
   XML[] children = xml.getChildren("path");
   
   paths = new Vector<Vector<PVector>>();
   
   for (int i = 0; i < children.length; i++) {
-    println("path : ");
     String content = children[i].getString("d");
     content = content.replace("M", "");
     content = content.replace("L", "");
@@ -24,7 +25,8 @@ void setup() {
     while(tokens.hasMoreTokens()){
         Float x =  Float.parseFloat(tokens.nextToken());
         Float y =  Float.parseFloat(tokens.nextToken());
-        PVector p = new PVector(x,y);
+        Float c = random(0,255);
+        PVector p = new PVector(x,y,c);
         pvs.add(p);
         
     }
@@ -41,13 +43,18 @@ void draw() {
   fill(0,0,0);
   text(""+slow_draw_index, 120,120);
   int draw_counter = 0;
+  int path_counter = 0;
   outer:
   for(Vector<PVector> path : paths) {
+    
     Enumeration<PVector> path_it = path.elements();
     PVector p1 = path_it.nextElement();
+    stroke(color(127*(path_counter%3),127*((path_counter+1)%3),127*((path_counter+2)%3)));
     while (path_it.hasMoreElements()) {
        PVector p2 = path_it.nextElement();
+
        line(p1.x, p1.y, p2.x, p2.y);
+       
        p1= p2;
        draw_counter ++;
        if (draw_counter == slow_draw_index) {
@@ -55,7 +62,7 @@ void draw() {
          break outer;
        }
     }
-    
+    path_counter++;
   }
   delay(500);
   
